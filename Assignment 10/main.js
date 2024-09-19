dd087923c4bc113d958c281d67da8e056494b2e9
Muhammad Junaid Khan <mjunaidkhan996@outlook.com>
2024-09-19 19:34:33 +0500

    Upload Assignment 10 updated (JS 01)

---
+++ Assignment 10/main.js
@@ -0,0 +1,68 @@
+// CHAPTER #01
+alert("Welcome to JS Assignment 01 by Junaid Khan");
+
+function validateLogin() {
+    alert("Error! Please enter a valid password.");
+}
+
+function showWelcomeMessage() {
+    alert("Welcome to JS Land...\nHappy coding!");
+}
+
+function showTwoAlerts() {
+    alert("Welcome to JS Land...");
+    alert("Happy Coding!");
+}
+
+console.log("Hello... I can run JS through my web browser's console");
+
+// CHAPTER #02
+var username;
+
+var myName = "Muhammad Junaid Khan"
+
+var message;
+message = "Hello World";
+
+function messageAlert() {
+    alert(message);
+}
+
+var studentName = "Haris";
+var studentAge = "22 years old";
+var studentDesignation = "Certified Android Developer"
+
+function studentBio() {
+    alert(studentName);
+    alert(studentAge);
+    alert(studentDesignation);
+}
+
+function showPizza() {
+    var word = "PIZZA";
+    alert(word + "\n" + word.slice(0, 4) + "\n" + word.slice(0, 3) + "\n" + word.slice(0, 2) + "\n" + word.slice(0, 1));
+}
+
+function showEmail() {
+    var email = "mjunaidkhan996@outlook.com";
+    alert("My email address is " + email);
+}
+
+function showBook() {
+    var book = "mjunaidkhan996@outlook.com";
+    alert("I'm trying to learn from the book " + book);
+}
+
+function specialChars() {
+    alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+}
+
+// CHAPTER #03
+var age = 26;
+
+// CHAPTER #04
+var a, b, c;
+
+var legal$, legal123, var02store, my$price, fix22$;
+
+// var $legal, 12legal, function, legal@23, legal-42;
